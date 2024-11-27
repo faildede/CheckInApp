a45f0bfc487d1a4dba22cddccb027cb3eb152413
faildede <justworkur@mail.ru>
2024-11-27 18:48:42 +0500

    Добавление поддержки CORS в основной файл приложения; обновление порта сервера с 3000 на 4000; добавление игнорирования временных данных в .gitignore; создание новых логов и конфигурационных файлов для MongoDB

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Включение CORS
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder() 
-  .setTitle('API')
-  .setDescription('API description')
-  .setVersion('1.0')
-  .addTag('API')
-  .build();
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
